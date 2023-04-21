57ac094116697b634c81444e06bf5334f3816867
Supun S Dananjaya <Supun S Dananjaya@DESKTOP-LRUHF90.mshome.net>
2023-04-21 23:41:18 +0530

    Search with google automatically

--- src/basic/OpenGoogle.java
+++ src/basic/OpenGoogle.java
@@ -1,5 +1,7 @@
 package basic;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,7 +11,11 @@ public class OpenGoogle {
 		
 		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\Chromedriver\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.google.com");
+		driver.get("https://www.google.com/");
+		
+		//enter a search 
+		driver.findElement(By.name("q")).sendKeys("jeasus"+Keys.ENTER);
+		
 
 	}
 
