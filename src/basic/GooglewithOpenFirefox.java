86d39fde57f5ffc49f3ccb02a6806acc67285443
Supun S Dananjaya <Supun S Dananjaya@DESKTOP-LRUHF90.mshome.net>
2023-04-21 23:36:15 +0530

    Run Google with Friefox browser

---
+++ src/basic/GooglewithOpenFirefox.java
@@ -0,0 +1,17 @@
+package basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GooglewithOpenFirefox {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		//System.setProperty("webdriver.chrome.driver", "C:\\Selenium Webdriver\\Chromedriver\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.gecko.driver", "C:\\Selenium Webdriver\\FireFoxdriver\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(5000);
+	}
+
+}
